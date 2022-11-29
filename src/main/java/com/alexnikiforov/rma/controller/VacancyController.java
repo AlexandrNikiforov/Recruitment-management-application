76c25213105bb350fab94446ce6736d0b2a057e5
alexandr.nikiforov <nikiforov.san.sanich@gmail.com>
2022-11-29 10:46:22 +0400

    Add extra Vacancy endpoints

--- src/main/java/com/alexnikiforov/rma/controller/VacancyController.java
+++ src/main/java/com/alexnikiforov/rma/controller/VacancyController.java
@@ -6,13 +6,18 @@ import com.alexnikiforov.rma.services.VacancyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/vacancy")
+@RequestMapping("/api/v1/vacancies")
 @Slf4j
 @AllArgsConstructor
 public class VacancyController {
@@ -24,7 +29,24 @@ public class VacancyController {
         //TODO add validation
         Vacancy vacancy = vacancyService.addVacancy(vacancyDto);
         return ResponseEntity.ok().body(vacancy);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<Vacancy>> getVacancies() {
+        List<Vacancy> vacancies = vacancyService.getAll();
+        return ResponseEntity.ok().body(vacancies);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Vacancy> getVacancyById(@PathVariable("id") Long id) {
+        Vacancy vacancy = vacancyService.getById(id);
+        return ResponseEntity.ok().body(vacancy);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<String> deleteVacancyById(@PathVariable("id") Long id) {
+        vacancyService.deleteById(id);
+        return ResponseEntity.ok().body("Deleted vacancy with id = " + id);
     }
 
 }
--- src/main/java/com/alexnikiforov/rma/services/VacancyService.java
+++ src/main/java/com/alexnikiforov/rma/services/VacancyService.java
@@ -6,7 +6,10 @@ import com.alexnikiforov.rma.repositories.VacancyRepository;
 import com.alexnikiforov.rma.util.DtoUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -20,4 +23,19 @@ public class VacancyService {
         return vacancyRepository.save(DtoUtil.toVacancyEntity(vacancyDto));
     }
 
+    public List<Vacancy> getAll() {
+        log.info("Get all vacancies");
+        return vacancyRepository.findAll();
+    }
+
+    public Vacancy getById(Long id) {
+        log.info("Get vacancy by id : {}", id);
+        return vacancyRepository.findById(id)
+                .orElseThrow();
+    }
+
+    public void deleteById(Long id) {
+        log.info("Delete vacancy with id: {}", id);
+        vacancyRepository.deleteById(id);
+    }
 }
